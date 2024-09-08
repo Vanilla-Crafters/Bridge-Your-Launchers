ac4a1473ad931aa6054597ae37fc64d045c5637e
emirsassan <endwse@gmail.com>
2024-09-08 15:04:04 +0300

    remove flatlaf and use default java swing ui

--- src/main/java/net/vanillacrafters/bridgeyourlaunchers/container/Container.java
+++ src/main/java/net/vanillacrafters/bridgeyourlaunchers/container/Container.java
@@ -1,21 +1,16 @@
 package net.vanillacrafters.bridgeyourlaunchers.container;
 
-import com.formdev.flatlaf.FlatDarkLaf;
-
 import javax.swing.*;
 import java.awt.*;
 
 public class Container {
     private static void init() {
-        FlatDarkLaf.setup();
-
         final JPanel panel = new JPanel();
         panel.add(new JButton("FlatDarkLaf button!"));
         panel.add(new JTextField("FlatDarkLaf text field!"));
 
         final JFrame frame = new JFrame("FlatDarkLaf test.");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(new Dimension(1200, 700));
         frame.getContentPane().add(panel);
         frame.pack();
         frame.setLocationRelativeTo(null);
